8ed7cb863678939e89fda3f9e1f341d6438a00f0
Patrick Santana <psantana@moogu.com>
2019-08-26 15:02:53 +0200

    set as error

--- Java/src/main/java/com/gildedrose/validator/GoodsValidator.java
+++ Java/src/main/java/com/gildedrose/validator/GoodsValidator.java
@@ -13,19 +13,19 @@ public final class GoodsValidator {
 
 	private static void validateQualityNeverNegative(Item item) {
 		if (item.quality < 0) {
-			System.out.printf("Validation error: Quantity should not be negative [%s]", item);
+			System.err.printf("Validation error: Quantity should not be negative [%s]", item);
 		}
 	}
 
 	private static void validateSulfurasQuality(Item item) {
 		if (item.name.equals(Goods.SULFURAS.getName()) && item.quality != 80) {
-			System.out.printf("Validation error: %s should always have quality 80. [%s]", Goods.SULFURAS.getName(), item);
+			System.err.printf("Validation error: %s should always have quality 80. [%s]", Goods.SULFURAS.getName(), item);
 		}
 	}
 
 	private static void validateQualityMax(Item item) {
 		if (!item.name.equals(Goods.SULFURAS.getName()) && item.quality > 50) {
-			System.out.printf("Validation error: %s should always have quality below or equal 50. [%s]", item.name, item);
+			System.err.printf("Validation error: %s should always have quality below or equal 50. [%s]", item.name, item);
 		}
 	}
 }
